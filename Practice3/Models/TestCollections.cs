932fd455a8d583d028753720997bcafc99300ce8
Данила <kacher-2005@bk.ru>
2023-04-09 03:00:41 +0300

    Завершение третьей лабораборной

---
+++ Practice3/Models/TestCollections.cs
@@ -0,0 +1,58 @@
+﻿using Practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice3.Models
+{
+    public class TestCollections
+    {
+        public List<Person> TestTKeys;
+
+        public List<string> TestTString;
+
+        public Dictionary<Person, Student> TestDictionaryTKey;
+
+        public Dictionary<string, Student> TestDictionaryString;
+
+        private static Student _setedStudent; 
+
+        public TestCollections(int count)
+        {
+            TestTKeys = new List<Person>();
+            TestTString = new List<string>();
+            TestDictionaryTKey = new Dictionary<Person, Student>();
+            TestDictionaryString = new Dictionary<string, Student>();
+
+            for (int i = 0; i < count; i++)
+            {
+                TestTKeys.Add(new Person());
+                TestTString.Add(new Person().ToString());
+                TestDictionaryString.Add(new Person().ToString(), new Student());
+                TestDictionaryTKey.Add(new Person(), new Student());
+            }
+        }
+
+        public static Student SetStudentForTestCollection(int count)
+        {
+            _setedStudent = new Student();
+            return _setedStudent;
+        }
+
+
+        public string GetTimeForFirstItem()
+        {
+            var response = "";
+
+            var startTest1 = Environment.TickCount64;
+            var firstItem = TestTKeys.First();
+            var endTest1 = Environment.TickCount64;
+            response += "Первый элемент типа List<Person> был найден за " + (endTest1 - startTest1) + "ms";
+
+
+            return response;
+        }
+    }
+}
--- Practice3/Program.cs
+++ Practice3/Program.cs
@@ -1,10 +1,32 @@
 ﻿
+using Practice3.Models;
 using System.Collections;
 
 public class Program
 {
     static void Main(string[] args)
     {
+        var studentCollection = new StudentCollection();
+        studentCollection.AddDefaults(10);
+        Console.WriteLine(studentCollection.ToString() + "\n");
+
+        Console.ResetColor();
+        studentCollection.SortByLastName();
+        Console.WriteLine("Сортировка по фамилии:" + studentCollection.ToString());
+
+        studentCollection.SortByDate();
+        Console.WriteLine("Сортировка по дате: " + studentCollection.ToString());
+
+        studentCollection.SortByGrage();
+        Console.WriteLine("Сортировка по оценке:" + studentCollection.ToString());
+
+        Console.WriteLine();
+        Console.WriteLine("Максимальный средний балл: " + studentCollection.MaxGrade);
+        Console.WriteLine("Специалисты: \n" + String.Join("\n", studentCollection.StudentsWithSpecialist.Select(c => c.ToString())));
+        Console.WriteLine("Группировка по сред баллу: \n" + String.Join("\n", studentCollection.AverageMarkGroup(3).Select(c => c.ToString())));
+
+        var test = new TestCollections(1000);
+        Console.WriteLine("\n\n" + test.GetTimeForFirstItem());
     }
 }
 
